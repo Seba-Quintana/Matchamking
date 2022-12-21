44b653212b434ee4d57ddf2390ee667c79d8c830
Emiliano Silva <emisilvacab@gmail.com>
2022-12-21 01:11:35 -0300

    Entities: Equipo, Jugador, Partido

---
+++ backend/src/Models/Equipo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Entities 
+{
+    public class EquipoEntity
+    {
+        public int Goles { get; set; }
+        public bool Suplentes { get; set; }
+        public List<JugadorEntity> Jugadores { get; set; }
+    }
+}
---
+++ backend/src/Models/Partido.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Entities
+{
+    public class Partido
+    {
+        public DateTime Fecha { get; set; }
+        public string Cancha { get; set; }
+        public Equipo Ganador { get; set; }
+        public Equipo Perdedor { get; set; }
+    }
+}
