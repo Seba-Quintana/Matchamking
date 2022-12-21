5b3573799aa81a89a89e4879eb599aa60c6cfb0a
Emiliano Silva <emisilvacab@gmail.com>
2022-12-21 01:26:37 -0300

    Equipo arreglo

--- backend/src/Models/Equipo.cs
+++ backend/src/Models/Equipo.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 namespace Entities 
 {
-    public class EquipoEntity
+    public class Equipo
     {
         public int Goles { get; set; }
         public bool Suplentes { get; set; }
-        public List<JugadorEntity> Jugadores { get; set; }
+        public List<Jugador> Jugadores { get; set; }
     }
 }
