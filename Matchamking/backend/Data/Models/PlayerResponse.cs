66bfbd08ad7a85c467f83da807a23335917382fd
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-12-24 19:02:37 -0300

    update response

--- Matchamking/backend/Data/Models/PlayerResponse.cs
+++ Matchamking/backend/Data/Models/PlayerResponse.cs
@@ -2,20 +2,21 @@
 using System.Collections.Generic;
 
 namespace backend.Data.Models
-{	
+{
 	public class PlayerResponse<T>
 	{
 		private string stsCod;
 		private string stsMsg;
 		private List<T> bodyResponseList;
 
-		public string StsCod { get { return stsCod; } set { stsCod = value; } }
-		public string StsMsg { get { return stsMsg; } set { stsCod = value; } }
+		public string StsCod { get { return stsCod; } private set { stsCod = value; } }
+		public string StsMsg { get { return stsMsg; } private set { stsCod = value; } }
 		public string BodyResponseList { get; set; }
 
 		public PlayerResponse()
 		{
 		}
+
 		public PlayerResponse(string stsCod, string stsMsg, List<T> bodyResponseList)
 		{
 			this.stsCod = stsCod;
