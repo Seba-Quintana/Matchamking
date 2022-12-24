ae5dd294290a1068d94478a8eb270276de13a999
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-12-24 02:38:37 -0300

    player wrapper

---
+++ Matchamking/backend/Data/Models/PlayerResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend.Data.Models
+{	
+	public class PlayerResponse<T>
+	{
+		private string stsCod;
+		private string stsMsg;
+		private List<T> bodyResponseList;
+
+		public string StsCod { get { return stsCod; } set { stsCod = value; } }
+		public string StsMsg { get { return stsMsg; } set { stsCod = value; } }
+		public string BodyResponseList { get; set; }
+
+		public PlayerResponse()
+		{
+		}
+		public PlayerResponse(string stsCod, string stsMsg, List<T> bodyResponseList)
+		{
+			this.stsCod = stsCod;
+			this.stsMsg = stsMsg;
+			this.bodyResponseList = bodyResponseList;
+		}
+	}
+}
