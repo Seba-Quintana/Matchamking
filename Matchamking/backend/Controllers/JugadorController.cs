273806835461eebe7b5ad53784b91817814418ff
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-12-26 02:48:47 -0300

    put post delete player y arreglos

--- Matchamking/backend/Controllers/JugadorController.cs
+++ Matchamking/backend/Controllers/JugadorController.cs
@@ -21,37 +21,38 @@ namespace backend.Controllers
 		}
 
 		// GET api/<ValuesController>/5
-		[HttpGet("")]
+		[HttpGet("{name}")]
+		public Task<JugadorResponse<Jugador>> GetPlayer([FromQuery(Name = "name")] string name)
+		{
+			return _jugadorServices.GetPlayer(name);
+		}
+
+		// GET api/<ValuesController>/5
+		[HttpGet]
 		public async Task<JugadorResponse<Jugador>> GetPlayers()
 		{
 			return await _jugadorServices.GetPlayers();
 		}
 
-		// GET api/<ValuesController>/5
-		[HttpGet("{name}")]
-		public Task<JugadorResponse<Jugador>> GetPlayer(string name)
-		{
-			return _jugadorServices.GetPlayer(name);
-		}
-
-		/*
 		// POST api/<ValuesController>
-		[HttpPost("{id}")]
-		public void PostPlayer([FromQuery] string name)
+		[HttpPost]
+		public Task<JugadorResponse<Jugador>> PostPlayer([FromQuery(Name = "name")] string name)
 		{
 			return _jugadorServices.PostPlayer(name);
 		}
-		*/
-		/*// DELETE api/<ValuesController>/5
-		[HttpDelete("{id}")]
-		public void Delete(int id)
+		
+		// DELETE api/<ValuesController>/5
+		[HttpDelete]
+		public Task<JugadorResponse<Jugador>> DeletePlayer([FromQuery(Name = "name")] string name)
 		{
+			return _jugadorServices.DeletePlayer(name);
 		}
-
+		
 		// PUT api/<ValuesController>/5
-		[HttpPut("{id}")]
-		public void Put(int id, [FromBody] string value)
+		[HttpPut]
+		public Task<JugadorResponse<Jugador>> PutEloboost([FromQuery] string name, [FromQuery] float eloboost)
 		{
-		}*/
+			return _jugadorServices.PutEloboost(name, eloboost);
+		}
 	}
 }
